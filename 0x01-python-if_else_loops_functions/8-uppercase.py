d15069d6d7da3e5177d004e3c476e4e946fca8b0
Brian Chew <bhchew@ucdavis.edu>
2018-04-11 06:29:35 +0000

    1st commit

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def uppercase(str):
+    upper = ""
+
+    for i in str:
+        if 'a' <= i and i <= 'z':
+            strIndex = ord(i) - ord('a')
+            ordVal = strIndex + ord('A')
+            i = chr(ordVal)
+        upper = upper + i
+
+    print(upper, end="\n")
+    return upper
