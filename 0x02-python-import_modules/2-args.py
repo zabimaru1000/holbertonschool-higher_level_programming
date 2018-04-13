8e62437dfcd64a5ad63c509af96994f0589c0952
Brian Chew <bhchew@ucdavis.edu>
2018-04-13 01:01:25 +0000

    1st commit

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys, getopt
+    ac = len(sys.argv)
+    j = 1
+
+    if ac == 1:
+        print("{:d} arguments.".format(ac - 1))
+
+    else:
+        if ac == 2:
+            print("{:d} argument:".format(ac - 1))
+        elif ac > 2:
+            print("{:d} arguments:".format(ac - 1))
+
+    for i in range(1, ac):
+        print("{:d}: {:s}".format(j, sys.argv[i]))
+        j = j + 1
