0d2cf514cd947f3be282d1f066f1b962c63d8c67
Brian Chew <bhchew@ucdavis.edu>
2018-04-13 03:51:45 +0000

    1st commit

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+    import sys
+    a = dir(hidden_4)
+
+    print("{}".format(a[8]))
+    print("{}".format(a[9]))
+    print("{}".format(a[10]))
