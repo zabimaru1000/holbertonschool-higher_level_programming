4c8f44c5984cead958ce24a4bd3cb1df88cc1141
Brian Chew <bhchew@ucdavis.edu>
2018-04-18 00:39:39 +0000

    Changed loop condition to reversed

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    start = my_list[-1]
-    end = my_list[0] - 1
-    for i in range(start, end, -1):
+    for i in reversed(my_list):
         print("{:d}".format(i))
