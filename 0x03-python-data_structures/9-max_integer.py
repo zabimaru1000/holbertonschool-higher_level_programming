7dc0797c2d576b922fa42b6474e21d88ae7c863e
Brian Chew <bhchew@ucdavis.edu>
2018-04-18 02:29:28 +0000

    Added empty list condition, and removed comments

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    #initialize variable to the first element of list
-    max = my_list[0]
+    if my_list == []:
+        return None
 
-    #Until i reaches the last element of list, compare two numbers
+    max = my_list[0]
     for i in range(len(my_list)):
         if max < my_list[i]:
             max = my_list[i]
