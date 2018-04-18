3870faf2a35dc78338a02c0b5fddadcef6fb9380
Brian Chew <bhchew@ucdavis.edu>
2018-04-18 02:17:28 +0000

    Looking for simpler way to implement separator

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,14 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
 
     for i in matrix:
-        sep = 0
         for j in i:
-            if sep == len(i) - 1:
-                print("{:d}".format(j), end="")
-
-            else:
-                print("{:d}".format(j), end=" ")
-
-            sep += 1
+                print("{:d}".format(j), end = " ")
 
         print("")
