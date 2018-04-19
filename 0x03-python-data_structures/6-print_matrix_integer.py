3b5f427aba5796b425b67a352f02951531c588a2
Brian Chew <bhchew@ucdavis.edu>
2018-04-19 05:14:46 +0000

    Added conditions where if the digits were not the last digit

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,15 @@
 def print_matrix_integer(matrix=[[]]):
 
     for i in matrix:
+        digit_count = 0
+
         for j in i:
+            if digit_count < len(i) - 1:
                 print("{:d}".format(j), end=" ")
+
+            else:
+                print("{:d}".format(j), end="")
+
+            digit_count = digit_count + 1
 
         print("")
