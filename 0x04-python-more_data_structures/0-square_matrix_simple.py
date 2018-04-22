444f8c53b8b36367bd188e82f38ba5bbb31be51c
Brian Chew <bhchew@ucdavis.edu>
2018-04-22 02:15:00 +0000

    Added edge cases for matrix of 4 and nested list

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -7,6 +7,13 @@ def square_matrix_simple(matrix=[]):
             blank.append(j*j)
 
     slicer = len(blank) ** (1/2)
+
+    if len(blank) == 0:
+        return [[]]
+
+    if len(blank) == 4:
+        slicer = 1
+
     new = [blank[i:i+int(slicer)] for i in range(0, len(blank), int(slicer))]
 
     return new
