057e85c6a271db39841575fa76295f0d81a5143a
Brian Chew <bhchew@ucdavis.edu>
2018-05-01 05:31:21 +0000

    1st commit

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        for i in range(0, x):
+            print(my_list[i], end = "")
+
+        print()
+        return x
+
+    except IndexError:
+        print()
+        return max(my_list)
---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    c = 0
+
+    try:
+        c = a / b
+        return c
+
+    except ZeroDivisionError:
+        return None
+
+    finally:
+        print("Inside result: {}".format(c))
