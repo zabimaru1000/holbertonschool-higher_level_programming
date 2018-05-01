44d72857942ac1b41b8419b51d7e9bfa7ddec444
Brian Chew <bhchew@ucdavis.edu>
2018-05-01 05:38:16 +0000

    fixed pep8

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 def safe_print_list(my_list=[], x=0):
     try:
         for i in range(0, x):
-            print(my_list[i], end = "")
+            print(my_list[i], end="")
 
         print()
         return x
