b17974bec3540f447e2b81adeb285455c8cc16f6
Brian Chew <bhchew@ucdavis.edu>
2018-05-02 06:48:09 +0000

    Changed order

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,11 +4,10 @@
 
 class Square:
     def __init__(self, size=0):
+        self.__size = size
         try:
             if size < 0:
                 raise ValueError("size must be >= 0")
-            self.__size = size
 
         except TypeError:
-            if type(size) != int:
                 raise TypeError("size must be an integer")
