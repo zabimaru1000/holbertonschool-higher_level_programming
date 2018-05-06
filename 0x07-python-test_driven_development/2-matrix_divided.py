aec312430a5d4c8857d67b2106edf72de7cfe7a7
Brian Chew <bhchew@ucdavis.edu>
2018-05-06 06:46:34 +0000

    1st commit

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
+"""
+Module for matrix_divided
+Uses one function divide all integers in 2D list
+"""
+
+
 def matrix_divided(matrix, div):
-    return [[i / 2 for i in j] for j in matrix]
+    """
+    Returns result of all ints divided by div
+    """
+
+    result = [[round(i / div, 2) for i in j] for j in matrix]
+
+    return result
