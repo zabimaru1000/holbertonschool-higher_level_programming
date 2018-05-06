39f9f6a1667d9fc508471cf93f72794e484c93d1
Brian Chew <bhchew@ucdavis.edu>
2018-05-06 06:05:06 +0000

    Added documentation

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
+"""
+Module for add_integer
+Uses one function to add two integers
+
+"""
+
+
 def add_integer(a, b=98):
+    """
+    Returns sum of integers a and b
+
+    """
+
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
 
