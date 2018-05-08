2d0c263c47b0d2133329995f2ed83b965a388bb9
Brian Chew <bhchew@ucdavis.edu>
2018-05-08 06:16:52 +0000

    Added documentation

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
+"""
+Creates a class Square
+"""
+
+
 class Square:
+    """
+    Pass through the class
+    """
+
     pass
--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,15 +1,28 @@
 #!/usr/bin/python3
+"""
+Creates a class Rectangle
+"""
+
 class Rectangle:
     def __init__(self, width=0, height=0):
+        """
+        Method that initiates height and width
+        """
         self.__height = height
         self.__width = width
 
     @property
     def height(self):
+        """
+        Gets property for height
+        """
         return self.height
 
     @height.setter
     def height(self, value):
+        """
+        Method to set height
+        """
         if not isinstance(self.__height, int):
             raise TypeError("height must be an integer")
         if self.__height < 0:
@@ -19,10 +32,16 @@ class Rectangle:
 
     @property
     def width(self):
+        """
+        Method to set width
+        """
         return self.width
 
     @width.setter
     def width(self, value):
+        """
+        Method to get width
+        """
         if not isinstance(self.__width, int):
             raise TypeError("width must be an integer")
         if self.__width < 0:
