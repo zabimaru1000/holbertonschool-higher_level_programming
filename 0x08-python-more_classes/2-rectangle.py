35eb0517b1989d33e04f63ded2515bf7579df0d1
Brian Chew <bhchew@ucdavis.edu>
2018-05-09 20:44:06 +0000

    Changed get and set values

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -17,16 +17,16 @@ class Rectangle:
         """
         Method to get height
         """
-        return self.height
+        return self.__height
 
     @height.setter
     def height(self, value):
         """
         Method to set height
         """
-        if not isinstance(self.__height, int):
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if self.__height < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         else:
             self.__height = value
@@ -36,16 +36,16 @@ class Rectangle:
         """
         Method to set width
         """
-        return self.width
+        return self.__width
 
     @width.setter
     def width(self, value):
         """
         Method to get width
         """
-        if not isinstance(self.__width, int):
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if self.__width < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
 
         else:
