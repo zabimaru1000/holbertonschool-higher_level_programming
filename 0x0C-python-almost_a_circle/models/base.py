3ae37a74eb03b94b4e89fb8ab577808d953cb64f
Brian Chew <bhchew@ucdavis.edu>
2018-05-26 02:47:17 +0000

    added documentation

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,9 +5,15 @@ Class Base
 
 
 class Base:
+    """
+    Base class
+    """
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """
+        Class constructor init
+        """
         if id is not None:
             self.id = id
         else:
