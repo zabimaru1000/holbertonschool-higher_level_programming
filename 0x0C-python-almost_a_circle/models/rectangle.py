56cfa51363d4ea292b77aa289daa441eba7461d8
Brian Chew <bhchew@ucdavis.edu>
2018-05-26 03:38:19 +0000

    Added instance area

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -92,3 +92,9 @@ class Rectangle(Base):
         elif value < 0:
             raise ValueError("y must be > 0")
         self.__y = value
+
+    def area(self):
+        """
+        Returns area of rectangle
+        """
+        return self.__width * self.__height
