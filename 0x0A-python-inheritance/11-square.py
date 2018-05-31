cbe79909060afe9fa5cf0a2ced357d6317b74f85
Brian Chew <bhchew@ucdavis.edu>
2018-05-31 16:23:58 +0000

    1st commit

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Class Square
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    def __init__(self, size):
+        self.__size = size
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+
+        def area(self):
+            return self.__size * self.__size
+
+        def str(self):
+            str_rep = "[Square] {}/{}".format(self.__size, self.__size)
+            return str_rep
